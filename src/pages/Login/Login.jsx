cbffb6d5e3df0b55c2a2538c692ac17a8eedf33a
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-10 15:28:41 +0200

    ♻️ F02:LOGIN >Create LoginForm

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,44 +1,8 @@
-import { useState } from "react";
+import LoginForm from "../../componnents/LoginForm";
 import "./Login.css";
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Bonjour ${username}`);
-        setUsername("");
-    };
-    return (
-        <main className="loginContainer">
-            <h1 className="loginH1">Bienvenue chez nous!</h1>
-            <h2 className="loginH2">Connectez-vous</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    value={username}
-                    placeholder="Entre votre prénom..."
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <button type="submit">Accédez à votre espace</button>
-            </form>
-        </main>
-    );
+    return <LoginForm />;
 }
 
 export default Login;
