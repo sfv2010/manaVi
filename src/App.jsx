93df37a2940af18efb0ca209164352ef4a6971a7
Sayaka Vincent <sfv2010@hotmail.fr>
2023-11-13 14:50:14 +0100

    🔥 F02-2: removed Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Order from "./pages/Order/OrderPage";
 import Error from "./pages/Error/ErrorPage";
@@ -9,8 +8,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/login" element={<Login />}></Route>
+                <Route path="/" element={<Login />}></Route>
                 <Route path="/order" element={<Order />}></Route>
                 <Route path="*" element={<Error />}></Route>
             </Routes>
